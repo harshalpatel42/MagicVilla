d25c526009be9492ca531bdc34ff176ce56a9c2d
Harshal <harshalpatel843@gmail.com>
2025-03-12 15:21:53 +0530

    added VillaNumberService Interface and Impemneted Service class for mapping the reuqests

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -10,8 +10,8 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddHttpClient<IVillaService, VillaService>();
 builder.Services.AddScoped<IVillaService, VillaService>();
 
-// builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
-// builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
+builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
+builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
 var app = builder.Build();
 
