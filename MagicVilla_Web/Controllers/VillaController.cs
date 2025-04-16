96cc922471003c937af6c42332f579567d3f0e4c
Harshal <harshalpatel843@gmail.com>
2025-04-16 17:27:31 +0530

    added sweet alert for create,update and delete

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -46,9 +46,11 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.CreateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa Created Successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["Error"] = "Error Encountered";
             return View(model);
         }
 
@@ -74,9 +76,11 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa Updated Successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["Error"] = "Error Encountered";
             return View(model);
         }
 
@@ -99,8 +103,10 @@ namespace MagicVilla_Web.Controllers
             var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
             if (response != null && response.IsSuccess)
             {
+                TempData["success"] = "Villa Deleted Successfully";
                 return RedirectToAction(nameof(IndexVilla));
             }
+            TempData["Error"] = "Error Encountered";
             return View(model);
         }
     }
